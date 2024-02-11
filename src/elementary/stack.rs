68e2940089936ea8e8c097af0e51ab0b1c2f279e
Romain Leroux <romain@leroux.dev>
2024-02-11 19:18:34 +0100

    Add VecStack

--- src/elementary/stack.rs
+++ src/elementary/stack.rs
@@ -41,6 +41,44 @@ impl<T, const N: usize> ArrayStack<T, N> {
     }
 }
 
+pub struct VecStack<T> {
+    stack: Vec<T>,
+}
+
+impl<T> Default for VecStack<T> {
+    fn default() -> Self {
+        Self { stack: vec![] }
+    }
+}
+
+impl<T> VecStack<T> {
+    pub fn with_capacity(size: usize) -> Self {
+        Self {
+            stack: Vec::with_capacity(size),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.stack.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.stack.len()
+    }
+
+    pub fn push(&mut self, val: T) {
+        self.stack.push(val);
+    }
+
+    pub fn pop(&mut self) -> T {
+        self.stack.pop().expect("attempt to pop an empty stack")
+    }
+
+    pub fn peek(&self) -> &T {
+        self.stack.last().expect("attempt to peek an empty stack")
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -83,5 +121,37 @@ mod tests {
         let mut stack: ArrayStack<usize, 1> = ArrayStack::default();
         stack.push(1);
         stack.push(2);
+    }
+
+    #[test]
+    fn vec_stack_ok() {
+        let mut stack = VecStack::default();
+        stack.push(3);
+        stack.push(2);
+        stack.push(1);
+        assert_eq!(1, stack.pop());
+
+        stack.pop();
+        stack.pop();
+        assert!(stack.is_empty());
+
+        let range = 4..=9;
+        for i in range.clone() {
+            stack.push(i);
+        }
+        for i in range.rev() {
+            assert_eq!(i, stack.pop());
+        }
+        assert!(stack.is_empty());
+    }
+
+    #[test]
+    #[should_panic(expected = "attempt to pop an empty stack")]
+    fn vec_stack_panic_underflow() {
+        let mut stack = VecStack::default();
+        stack.push(1);
+        stack.pop();
+        assert!(stack.is_empty());
+        stack.pop();
     }
 }
