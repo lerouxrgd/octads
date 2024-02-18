dfea2419451d59cfe9c8ba36711d2e06ec239c3a
Romain Leroux <romain@leroux.dev>
2024-02-18 20:19:45 +0100

    Add LinkedListQueue

--- src/elementary/queue.rs
+++ src/elementary/queue.rs
@@ -1,5 +1,7 @@
 use alloc::alloc::{alloc, dealloc, handle_alloc_error, Layout};
-use core::ptr;
+use core::{mem::MaybeUninit, ptr};
+
+use crate::allocator::{BlockAllocator, Node};
 
 #[derive(Debug)]
 pub struct ChunkQueue<T> {
@@ -77,6 +79,77 @@ impl<T> Drop for ChunkQueue<T> {
     }
 }
 
+#[derive(Debug)]
+pub struct LinkedListQueue<T> {
+    allocator: BlockAllocator<T>,
+    len: usize,
+    remove: *mut Node<T>,
+    insert: *mut Node<T>,
+}
+
+impl<T> LinkedListQueue<T> {
+    pub fn new(block_size: usize, blocks_cap: usize) -> Self {
+        Self {
+            allocator: BlockAllocator::new(block_size, blocks_cap),
+            len: 0,
+            remove: ptr::null_mut(),
+            insert: ptr::null_mut(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    pub fn enqueue(&mut self, val: T) {
+        let tmp = self.allocator.get_node();
+        unsafe { (*tmp).val = MaybeUninit::new(val) };
+        if !self.is_empty() {
+            unsafe { (*self.insert).next = tmp };
+            self.insert = tmp;
+        } else {
+            self.remove = tmp;
+            self.insert = tmp;
+        }
+        self.len += 1;
+    }
+
+    pub fn dequeue(&mut self) -> T {
+        assert!(!self.is_empty(), "underflow: dequeuing from an empty queue");
+        let tmp = self.remove;
+        unsafe {
+            self.remove = (*tmp).next;
+            let val = (*tmp).val.assume_init_read();
+            self.allocator.return_node(tmp);
+            self.len -= 1;
+            val
+        }
+    }
+
+    pub fn peek(&self) -> &T {
+        assert!(!self.is_empty(), "underflow: peeking at an empty queue");
+        unsafe { (*self.remove).val.assume_init_ref() }
+    }
+}
+
+impl<T> Drop for LinkedListQueue<T> {
+    fn drop(&mut self) {
+        let mut next = self.remove;
+        while !next.is_null() {
+            let tmp = next;
+            unsafe {
+                (*tmp).val.assume_init_drop();
+                next = (*tmp).next;
+                self.allocator.return_node(tmp);
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -123,5 +196,41 @@ mod tests {
         let mut q = ChunkQueue::new(1);
         q.enqueue(1);
         q.enqueue(2);
+    }
+
+    #[test]
+    fn linked_list_queue_ok() {
+        let mut q = LinkedListQueue::new(2, 1);
+        q.enqueue(3);
+        q.enqueue(2);
+        q.enqueue(1);
+        assert_eq!(&3, q.peek());
+        assert_eq!(3, q.len());
+        assert_eq!(3, q.dequeue());
+
+        q.dequeue();
+        q.dequeue();
+        assert!(q.is_empty());
+
+        let range = 4..=9;
+        for (j, i) in range.clone().enumerate() {
+            assert_eq!(j, q.len());
+            q.enqueue(i);
+        }
+        assert_eq!(range.clone().count(), q.len());
+        for i in range {
+            assert_eq!(i, q.dequeue());
+        }
+        assert!(q.is_empty());
+    }
+
+    #[test]
+    #[should_panic(expected = "underflow: dequeuing from an empty queue")]
+    fn linked_list_queue_panic_underflow() {
+        let mut q = LinkedListQueue::new(4, 2);
+        q.enqueue(1);
+        q.dequeue();
+        assert!(q.is_empty());
+        q.dequeue();
     }
 }
